022c7455c1c30884c439726a5c79d0bae978d3d0
Isaac <isaacma45@gmail.com>
2022-04-22 17:37:01 +1200

    Update client.lua
    -Removed old code
    -Added comments
    -Removed test command
    -Removed unnecessary code/comments

--- client.lua
+++ client.lua
@@ -1,100 +1,63 @@
---[[
-function GetPlayers()
-	local players = {}
-
-	for i = 0, 10 do
-		if NetworkIsPlayerActive(1) then
-			table.insert(players, 1)
-		end
-	end
-	return players
-end
-
-Citizen.CreateThread(function()
-	local blips = {}
-	local currentPlayer = PlayerId()
-
-	while true do
-		Wait(100)
-
-		local players = GetPlayers()
-
-		for _, player in ipairs(GetActivePlayers()) do
-			if player ~= currentPlayer and NetworkIsPlayerActive(player) then
-				local playerPed = GetPlayerPed(player)
-				local playerName = GetPlayerName(player)
-				RemoveBlip(blips[player])
-				local new_blip = AddBlipForEntity(playerPed)
-				SetBlipNameToPlayerName(new_blip, player)
-				SetBlipColour(new_blip, 0)
-				SetBlipCategory(new_blip, 0)
-				SetBlipScale(new_blip, 0.85)
-				blips[player] = new_blip
-			end
-		end
-	end
-end)
-]]
-
--- Set the blip to a global variable
+-- Set the blips to a global array
 blips = {}
 -- Get player ID
 playerID = PlayerPedId()
 -- Get player name
 playerName = GetPlayerName(PlayerId())
 
-test = false
-
-RegisterCommand("test", function(source, args)
-	print(playerID)
-	print(playerName)
-end)
-
-
 -- Send the player pos to the server side script every 5 secs
 Citizen.CreateThread(function() 
 	while true do
-		-- this if statement crashes the game
-		--if test then
-			-- Wait 5 secs just for testing
-			Wait(5000)
+		-- Wait 5 secs just for testing
+		Wait(5000)
 
-			-- Get player pos
-			local pos = GetEntityCoords(playerID)
+		-- Get player pos
+		local pos = GetEntityCoords(playerID)
 
-			-- Trigger the server event playerPos
-			--print("Triggering Server Event: PlayerBlips:playerPos")
-			TriggerServerEvent("PlayerBlips:playerPos", pos.x, pos.y, pos.z, playerName)
-		--end
+		-- Trigger the server event playerPos
+		--print("Triggering Server Event: PlayerBlips:playerPos")
+		TriggerServerEvent("PlayerBlips:playerPos", pos.x, pos.y, pos.z, playerName)
 	end
 end)
 
 -- Get the player positions and update the map with the new coords
 RegisterNetEvent("PlayerBlips:updateBlips", function(x, y, z, name)
+	-- if the name of the incoming coords is the same as our local player ignore the coords
 	if (name == playerName) then
 		return 
 	end
 
+	-- get the local player position
 	local playerPos = GetEntityCoords(PlayerPedId())
+	-- get the targets player position
 	local targetPos = vector3(x, y, z)
 
+	-- if the blip exists in the blips array, remove it
 	if blips[name] then
 		RemoveBlip(blips[name])
 	end
 
+	-- calculate the distance between the two players
 	local distance = #(playerPos - targetPos)
 	
+	-- if the distance is more than 1000 units continue
 	if distance > 1000 then
+		-- calculate three random numbers between -150 and 150
 		local randomNumberX = math.random(-150, 150)
 		local randomNumberY = math.random(-150, 150)
 		local randomNumberZ = math.random(-150, 150)
 		
+		-- create the blip and add the random numbers to the coords of the player with radius 200(must have .0 on the end)
 		newBlip = AddBlipForRadius(x+randomNumberX, y+randomNumberY, z+randomNumberZ, 200.0)
+		-- set the blips color
 		SetBlipColour(newBlip, 1)
+		-- set the blips alpha
 		SetBlipAlpha(newBlip, 128)
 
+		-- add the blip to the blips array
 		blips[name] = newBlip
 	else
+		-- if the player is less than 1000 units away remove the blip
 		if blips[name] then
 			RemoveBlip(blips[name])
 		end
