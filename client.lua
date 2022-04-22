aadfbb5c6c50b7f31e31ba9b96a4b110e0e6b4c7
Isaac <isaacma45@gmail.com>
2022-04-22 14:50:06 +1200

    Update client.lua
    -Added global player id and name vars
    -Changed the global blip var to an array
    -Removed the local player id and name vars from the looped function
    -Added new vars to the TriggerServerEvent function
    -Added new vars to the RegisterNetEvent function
    -Added a check to see if the blip is the player itself and to ignore it (will comment this out for testing purposes)

--- client.lua
+++ client.lua
@@ -37,7 +37,11 @@ end)
 ]]
 
 -- Set the blip to a global variable
-blip = nil
+blips = {}
+-- Get player ID
+playerID = PlayerPedId()
+-- Get player name
+playerName = GetPlayerName(playerID)
 
 -- Send the player pos to the server side script every 5 secs
 Citizen.CreateThread(function() 
@@ -46,18 +50,17 @@ Citizen.CreateThread(function()
 		Wait(500)
 
 		-- Get player pos
-		local playerPed = PlayerPedId() -- get the local player ped
-		local pos = GetEntityCoords(playerPed) -- get the pos of the local player ped
+		local pos = GetEntityCoords(playerID)
 
 		-- Trigger the server event playerPos
 		print("Triggering Server Event: PlayerBlips:playerPos")
-		TriggerServerEvent("PlayerBlips:playerPos", pos.x, pos.y, pos.z)
+		TriggerServerEvent("PlayerBlips:playerPos", pos.x, pos.y, pos.z, playerName, playerID)
 	end
 end)
 
 -- Get the player positions and update the map with the new coords
-RegisterNetEvent("PlayerBlips:updateBlips", function(x, y, z)
-	RemoveBlip(blip)
+RegisterNetEvent("PlayerBlips:updateBlips", function(x, y, z, name, id)
+	RemoveBlip(blip[name])
 
 	local playerPos = GetEntityCoords(PlayerPedId())
 	local targetPos = vector3(-1147.27, 1000.00, 203.92)
@@ -67,6 +70,7 @@ RegisterNetEvent("PlayerBlips:updateBlips", function(x, y, z)
 	print(distance)
 
 	if distance > 30 then
+		if name == playerName then break end
 		blip = AddBlipForCoord(x, y, z)
 		SetBlipScale(blip, 0.9)
 		SetBlipSprite(blip, 364)
